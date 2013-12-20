9657568b663eecc521c72033ca973c2f06dd7bc3
Mattias Buelens <mattias@buelens.com>
2013-12-20 12:17:10 +0100

    Fixed IDP location.

--- instances-and-testing/playInteractive.sh
+++ instances-and-testing/playInteractive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/localhost/packages/eclipse/eclipse-4.3-ide/external/idp -e "playInteractive()" instances-and-testing.idp
+/localhost/packages/eclipse/eclipse-4.3-ide/idp3/bin/idp -e "playInteractive()" instances-and-testing.idp
 
 read -p "Press [Enter] to exit..."
--- instances-and-testing/visualiseModels.sh
+++ instances-and-testing/visualiseModels.sh
@@ -5,6 +5,6 @@ nmodels=${1-1}
 echo "Visualizing $nmodels model(s)"
 echo
 
-/localhost/packages/eclipse/eclipse-4.3-ide/external/idp -e "visualiseModels($nmodels)" instances-and-testing.idp
+/localhost/packages/eclipse/eclipse-4.3-ide/idp3/bin/idp -e "visualiseModels($nmodels)" instances-and-testing.idp
 
 read -p "Press [Enter] to exit..."
